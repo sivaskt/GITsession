ce62ac03da0507c0f2e1eee2377d201800f8e84d
sivaskt <152521134+sivaskt@users.noreply.github.com>
2024-02-17 18:39:17 +0530

    Update AllCheckBoxes.java
    krishna

--- src/analysis/AllCheckBoxes.java
+++ src/analysis/AllCheckBoxes.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AllCheckBoxes
 {
-	//sai
+	//sai krishna
 //	@Test 
 //	public void allCheckBoxes()
 	public static void main(String[] args) 
